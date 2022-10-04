e88620a7f030cde44c32481fb7688362a3a62d69
Valentin Cabral <vcabral@oliver.local>
2022-10-04 12:47:08 -0300

    Avance con formulario transacciones

---
+++ ManejoPresupuesto/ManejoPresupuesto/Servicios/RepositorioTransacciones.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public interface IRepositorioTransacciones
+    {
+        Task Crear(Transaccion transaccion);
+    }
+    public class RepositorioTransacciones:IRepositorioTransacciones
+    {
+        private readonly string connectionString;
+        public RepositorioTransacciones(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task Crear(Transaccion transaccion)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var Id = await connection.QuerySingleAsync<int>($@"
+                    INSERT INTO Transacciones (UsuarioId, FechaTransaccion, Monto, Nota, CuentaId, CategoriaId)
+                    VALUES (@UsuarioId, @FechaTransaccion, ABS(@Monto), @Nota, @CuentaId, @CategoriaId)
+
+                    UPDATE Cuentas
+                    SET Balance += @Monto
+                    WHERE Id = @CuentaId
+
+                    SELECT SCOPE_IDENTITY();", transaccion);
+            transaccion.Id = Id;
+        }
+    }
+}
