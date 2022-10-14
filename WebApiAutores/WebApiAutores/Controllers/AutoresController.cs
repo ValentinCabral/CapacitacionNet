017380b578a8660227440d8f681822c7d4d03424
Valentin Cabral <vcabral@oliver.local>
2022-10-14 12:54:48 -0300

    Creacion del proyecto API Autores, limpieza del proyecto, instalacion de los nuggets (Microsoft sql y microsoft tools)

---
+++ WebApiAutores/WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]         // Para automatizar validaciones 
+    [Route("api/autores")]  //Ruta para acceder al controlador
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>()
+            {
+                new Autor() {Id = 1, Nombre = "Valentin"},
+                new Autor() {Id = 2, Nombre = "Tomas"}
+            };
+        }
+    }
+}
